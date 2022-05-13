f49c83ddb187028a33059f6dce602845ab04c29c
Ben David <66976120+bmdavid2@users.noreply.github.com>
2022-05-13 16:44:38 -0500

    add support for 96 well and 384 well plates

--- R/Mantis_Dispense.R
+++ R/Mantis_Dispense.R
@@ -3,16 +3,18 @@
 #' @param filename a filename for the dispense list. must end in ".dl.txt"
 #' @param reagent_layout a list of reagent volume matricies. see 'reagents_to_layout'
 #' @export
-export_mantis_worklist <- function(filename,reagent_layout){
+export_mantis_worklist <- function(filename,reagent_layout,platetype="breakaway_pcr_96"){
   num_reagents <- length(reagent_layout)
   delay_header <- c(num_reagents)
+  platefile <- c("breakaway_pcr_96.pd.txt","PT3-96-Assay.pd.txt","PT9-384-Assay.pd.txt")
+  names(platefile) <- c("breakaway_pcr_96","96-well","384-well")
 
   for (i in c(1:num_reagents)){
     delay_header <- c(delay_header,c(0,""))
   }
   outfile=file(filename,open="w",encoding = "UTF-8")
   write.table(c("[ Version: 5 ]"),file=outfile,append=FALSE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
-  write.table(c("breakaway_pcr_96.pd.txt"),file=outfile,append=TRUE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
+  write.table(c(platefile[platetype]),file=outfile,append=TRUE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
   write.table(t(delay_header),file=outfile,append=TRUE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
   write.table(t(c(1)),file=outfile,append=TRUE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
   write.table(t(delay_header),file=outfile,append=TRUE,row.names=FALSE,col.names=FALSE,na="",quote=FALSE,fileEncoding = "UTF-8",sep="\t",eol="\r\n")
@@ -28,16 +30,19 @@ export_mantis_worklist <- function(filename,reagent_layout){
 
 #' Convert an ordered reagent volume dataframe to a plate layout 
 #' 
-#' @param reagent_df reagent volumes in the order they should appear on the plate. Must only be reagent volumes
-#' @param platetype="96-well" specifies the plate type for the layout
+#' @param reagent_df reagent volumes in the order they should appear on the plate. Must only be reagent volumes.
+#' @param platetype="96-well" specifies the plate type for the layout. Must be either "96-well" or "384-well".
 #' @export
-reagents_to_layout<- function(reagent_df,platetype="96-well"){
-  if (platetype =="96-well"){
+reagents_to_layout<- function(reagent_df,platetype="breakaway_pcr_96"){
+  if (platetype =="96-well" || platetype== "breakaway_pcr_96"){
     nrows=8
     ncols=12
      
+  } else if (platetype=="384-well"){
+    nrows=16
+    ncols=24
   } else {
-    stop("Function can only handle 96 well plates right now")
+    stop("Function requires `96-well` or `384-well`")
   }
   n_reagents <- length(names(reagent_df))
   reagentnames <- names(reagent_df)
@@ -55,10 +60,27 @@ reagents_to_layout<- function(reagent_df,platetype="96-well"){
 #' 
 #' @param design_df Formatted dataframe with columns for each reagent, followed by either well location or row col 
 #' @param platetype="96-well" specifies the plate type for the design
-#' @param Well=TRUE if Well=TRUE, then use well designation, else combine row and col into well info
 #' @export
-design_to_reagents <- function(design_df,platetype="96-well",Well=TRUE){
-  if (platetype =="96-well"){
+design_to_reagents <- function(design_df,platetype="breakaway_pcr_96"){
+  welldict <- create_welldict(platetype = platetype)
+  design_df$Well <- paste(as.character(design_df$Row),as.character(design_df$Col),sep="")
+  reagentnames <- names(design_df)[1:(ncol(design_df)-3)]
+  unused_wells <- setdiff(names(welldict),design_df$Well)
+  n_unused_wells <- length(unused_wells)
+  expts_to_fill_plate <- data.frame(matrix(0,n_unused_wells,ncol(design_df)))
+  names(expts_to_fill_plate) <- names(design_df)
+  expts_to_fill_plate$Well <- unused_wells
+  design_df_filled <- rbind(design_df,expts_to_fill_plate)
+  ordered_design_df <- design_df_filled[order(factor(design_df_filled[,ncol(design_df_filled)],levels=wellnames)),] 
+  print(ordered_design_df)
+  reagent_df <- ordered_design_df[,c(1:(ncol(ordered_design_df)-3)),drop=FALSE]
+  names(reagent_df)=reagentnames
+  #print(reagent_df)
+  return(reagent_df)
+}
+
+create_welldict <- function(platetype="breakaway_pcr_96"){
+  if (platetype =="96-well" || platetype== "breakaway_pcr_96"){
     nrows=8
     ncols=12
     welldict <- c(1:96)
@@ -71,53 +93,40 @@ design_to_reagents <- function(design_df,platetype="96-well",Well=TRUE){
       }
     }
     names(welldict) <- wellnames
+  } else if (platetype=="384-well") {
+    nrows=16
+    ncol=24
+    welldict <- c(1:384)
+    wellnames <- c()
+    rownames <- c("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P")
+    colnames <- as.character(c(1:24))
+    for (i in colnames){
+      for (j in rownames){
+        wellnames <- c(wellnames,paste(j,i,sep=""))
+      }
+    }
+    names(welldict) <- wellnames
   } else {
-    stop("Function can only handle 96 well plates right now")
+    stop("Enter a valid plate type")
   }
-  omit <- 0
-  if (!Well){
-    design_df$Well <- paste(as.character(design_df[,(ncol(design_df)-1)]),as.character(design_df[,ncol(design_df)]),sep="")
-    omit <- 2
-  }
-  reagentnames <- names(design_df)[1:ncol(design_df)-omit-1]
-  unused_wells <- setdiff(wellnames,design_df$Well)
-  n_unused_wells <- length(unused_wells)
-  expts_to_fill_plate <- data.frame(matrix(0,n_unused_wells,ncol(design_df)))
-  names(expts_to_fill_plate) <- names(design_df)
-  expts_to_fill_plate$Well <- unused_wells
-  design_df_filled <- rbind(design_df,expts_to_fill_plate)
-  ordered_design_df <- design_df_filled[order(factor(design_df_filled[,ncol(design_df_filled)],levels=wellnames)),] 
-  print(ordered_design_df)
-  reagent_df <- ordered_design_df[,c(1:(ncol(ordered_design_df)-omit-1)),drop=FALSE]
-  names(reagent_df)=reagentnames
-  print(reagent_df)
-  return(reagent_df)
+  return(welldict)
 }
 
 #' Add well locations to a volumetric experimental design 
 #' 
 #' @param design Volumetric experimental design 
 #' @param filename="" Optional output file. Default outputs to the console
-#' @param platetype="96-well" Specifies the well naming scheme
+#' @param platetype="96-well" Must be either "96-well" or "384-well"
 #' @param randomize=FALSE If true, randomize the assigned wells. Else, put them in order.
 #' @export
-assign_wells <- function(design,filename="",platetype="96-well",randomize=FALSE){
-  wellnames <- c()
-  if (platetype =="96-well"){
-    rownames <- c("A","B","C","D","E","F","G","H")
-    colnames <- as.character(c(1:12))
-    for (i in colnames){
-      for (j in rownames){
-        wellnames <- c(wellnames,paste(j,i,sep=""))
-      }
-    }
-  } else {
-    stop("Function can only handle 96 well plates right now")
-  }
+assign_wells <- function(design,filename="",platetype="breakaway_pcr_96",randomize=FALSE){
+  welldict <- create_welldict(platetype=platetype)
   if (randomize){
-    wellnames <- sample(wellnames)
+    welldict <- sample(welldict)
   }
-  design$Well <- wellnames[1:nrow(design)]
+  design$Row <- names(welldict)[1:nrow(design)]
+  design$Col <- names(welldict)[1:nrow(design)]
+  design$Well <- names(welldict)[1:nrow(design)]
   write.csv(design,file=filename)
   return(design)
 }
@@ -125,16 +134,15 @@ assign_wells <- function(design,filename="",platetype="96-well",randomize=FALSE)
 #' 
 #' @param design Volumetric experimental design. Must include either Well or Row & Col columns
 #' @param name Design name. Funciton adds appropriate suffix automatically
-#' @param platetype="96-well" specifies the plate type 
-#' @param Well=TRUE must be adjusted based on the design's well information 
+#' @param platetype="96-well" specifies the plate type. Must be one of: \n "96-well" \n "breakaway_pcr_96" \n 384-well".
 #' @export
 #' 
-make_dispense_list <- function(design,name,platetype="96-well",Well=TRUE,...){
+make_dispense_list <- function(design,name,platetype="breakaway_pcr_96",...){
   kwargs=list(...)
   filename <- paste(name,".dl.txt")
-  reagents <- design_to_reagents(design,platetype=platetype,Well=Well,...)
+  reagents <- design_to_reagents(design,platetype=platetype,...)
   layout <- reagents_to_layout(reagents,platetype=platetype,...)
-  export_mantis_worklist(filename,layout)
+  export_mantis_worklist(filename,layout,platetype = platetype,...)
 }
 
 #test_reagents <- data.frame(A=matrix(0,96,1))
