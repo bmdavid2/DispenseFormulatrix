877ade2eab84fdb16e5f110d52ccced034e25448
Ben David <66976120+bmdavid2@users.noreply.github.com>
2022-07-27 14:45:53 -0400

    change dispense_list to mantis_dispense

--- R/Mantis_Dispense.R
+++ R/Mantis_Dispense.R
@@ -161,7 +161,7 @@ plateinfo <- function(platetype="breakaway_pcr_96"){
 #' @param platetype="breakaway_pcr_96" specifies the plate type. Must be one of: "96-well", "breakaway_pcr_96" , 384-well".
 #' @export
 #' 
-dispense_list <- function(design,name,platetype="breakaway_pcr_96"){
+mantis_dispense <- function(design,name,platetype="breakaway_pcr_96"){
   plate <- plateinfo(platetype)
   filename <- paste0(name,".dl.txt")
   reagents <- design_to_reagents(design,plate)
