d7bc09df93babe2c6c1cd5d95ce4b2238ca4e95c
Ben David <66976120+bmdavid2@users.noreply.github.com>
2022-12-13 12:03:46 -0500

    add juliacall dep

--- R/PlateLayout.R
+++ R/PlateLayout.R
@@ -7,22 +7,22 @@
 #' @param popsize the GA population size
 #' @export
 plate_layout <- function(design,factors,types,num_generations=50,popsize=30){
-  julia <- julia_setup()
-  julia_library("PlateLayout")
-  julia_library("DataFrames")
+  julia <- JuliaCall::julia_setup()
+  JuliaCall::julia_library("PlateLayout")
+  JuliaCall::julia_library("DataFrames")
   
   
-  julia_assign("design",design)
-  julia_assign("factors",factors)
-  julia_assign("types",types)
-  julia_assign("num_generations",num_generations)
-  julia_command("num_generations=Int(num_generations)")
-  julia_assign("popsize",popsize)
-  julia_command("popsize=Int(popsize);")
-  julia_eval("print(design);")
-  julia_command("plate=FactorAssignGA(design,factors,types;num_generations=num_generations,popsize=popsize);")
-  julia_command("out_designs=UpdateDesign(design,plate);")
-  out_designs=julia_eval("out_designs")
+  JuliaCall::julia_assign("design",design)
+  JuliaCall::julia_assign("factors",factors)
+  JuliaCall::julia_assign("types",types)
+  JuliaCall::julia_assign("num_generations",num_generations)
+  JuliaCall::julia_command("num_generations=Int(num_generations)")
+  JuliaCall::julia_assign("popsize",popsize)
+  JuliaCall::julia_command("popsize=Int(popsize);")
+  JuliaCall::julia_eval("print(design);")
+  juliaCall::julia_command("plate=FactorAssignGA(design,factors,types;num_generations=num_generations,popsize=popsize);")
+  JuliaCall::julia_command("out_designs=UpdateDesign(design,plate);")
+  JuliaCall::out_designs=julia_eval("out_designs")
   
   return(out_designs)
 }
